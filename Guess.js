179f8f66aad092bbc28afa602109a78ea57cb597
sgurubelli <79380174+sgurubelli@users.noreply.github.com>
2021-02-21 12:49:53 -0800

    Update Guess.js

--- Guess.js
+++ Guess.js
@@ -2,9 +2,9 @@ var number = Math.round(Math.random() * 100 + 1) ;
 var guess = prompt ("Guess a number between 1 and 100: ") ;
 
 if (guess > number)
-    console.log ("you are to high");
+    console.log ("you are too high");
 else if (guess < number)
-    console.log ("you are to low");
+    console.log ("you are too low");
 else {
   console.log ("Correct you win!");
 
